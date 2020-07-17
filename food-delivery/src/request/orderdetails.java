455beff2500fc210c49d7a1deae63e18a1aff7df
seenuvenkat <67901425+seenuvenkat@users.noreply.github.com>
2020-07-17 23:45:46 +0530

    Add files via upload

---
+++ food-delivery/src/request/orderdetails.java
@@ -0,0 +1,33 @@
+package fooddelivery.request;
+
+public class orderdetails {
+
+	private String[] items;
+	private float cost;
+	private String userId;
+
+	public float getCost() {
+		return cost;
+	}
+
+	public void setCost(float cost) {
+		this.cost = cost;
+	}
+
+	public String[] getItems() {
+		return items;
+	}
+
+	public void setItems(String[] items) {
+		this.items = items;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+}
